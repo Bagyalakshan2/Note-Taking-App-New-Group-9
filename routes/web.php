a4a4e56308b64d4b51a45bdb1274547d40c85ac2
Bhagya Lakshan <168979460+Bagyalakshan2@users.noreply.github.com>
2024-11-11 18:14:22 +0530

    Update web.php
    i added routes

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+Route::get('/', [NoteController::class, 'index'])->name('notes.index');
+Route::get('/notes/create', [NoteController::class, 'create'])->name('notes.create');
+Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
+Route::delete('/notes/{id}', [NoteController::class, 'destroy'])->name('notes.destroy');
+
+
+Route::get('/notes/{id}', [NoteController::class, 'show'])->name('notes.show');
+
+
+Route::get('/notes/{id}/edit', [NoteController::class, 'edit'])->name('notes.edit');
+Route::put('/notes/{id}', [NoteController::class, 'update'])->name('notes.update');
 
 Route::get('/', function () {
     return view('welcome');
